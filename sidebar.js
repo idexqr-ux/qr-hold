1665e9282d88877976e94d011fece13c17b8abaf
idexqr-ux <idexqr@outlook.com>
2026-05-17 09:04:03 +0100

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,1 +1,9 @@
+fetch("sidebar.html")
+  .then(response => response.text())
+  .then(html => {
+    const sidebar = document.getElementById("hold-sidebar");
 
+    if (sidebar) {
+      sidebar.innerHTML = html;
+    }
+  });
